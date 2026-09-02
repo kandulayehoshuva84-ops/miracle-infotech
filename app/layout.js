abd2967fdb18de1c7720635b1ccf2ea31d7266ec
kandulayehoshuva84-ops <kandulayehoshuva84@gmail.com>
2026-09-02 21:46:09 +0530

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'MIRACLE INFOTECH',description:'Internet Center Management Portal'};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
